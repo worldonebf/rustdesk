692b8c31296dab5b486399baeed8ab03f93e6af9
worldonebf <worldonebf@gmail.com>
2025-10-01 05:18:56 +0800

    修改common默认api地址为ssl链接

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rust.hjxpn.com:21114".to_owned()
+    "https://rust.hjxpn.com".to_owned()
 }
 
 #[inline]
